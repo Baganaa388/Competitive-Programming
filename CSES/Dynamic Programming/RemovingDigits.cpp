f0e099d4aecc6b61d53f40dc7a2b09bb84d765ad
Baganaa <baganaa237@gmail.com>
2026-04-23 18:04:13 +0800

    fixed

--- CSES/Dynamic Programming/RemovingDigits.cpp
+++ CSES/Dynamic Programming/RemovingDigits.cpp
@@ -1,21 +1,23 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int count = 0;
-    while(n != 0){
+    while (n != 0)
+    {
         int m = n;
         int max_element = 0;
-        while(m>0){
-            max_element = (m%10 >= max_element) ? (m%10) : max_element;
+        while (m > 0)
+        {
+            max_element = (m % 10 >= max_element) ? (m % 10) : max_element;
             m /= 10;
         }
         n -= max_element;
         count++;
     }
-    cout<<count<<endl;
+    cout << count << endl;
     return 0;
 }
