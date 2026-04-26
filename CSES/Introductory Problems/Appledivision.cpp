44499d723e0097cf99697c7539c97197621f67de
Baganaa <baganaa237@gmail.com>
2026-04-26 16:03:57 +0800

    fixed

--- CSES/Introductory Problems/Appledivision.cpp
+++ CSES/Introductory Problems/Appledivision.cpp
@@ -1,57 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+
+string to_binary(int number)
+{
+    int res[n] = {0};
+    int i = 0;
+    while (number > 0)
+    {
+        res[i] = (number % 2);
+        number /= 2;
+        i++;
+    }
+    string result;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (res[i] == 0)
+        {
+            result.push_back('0');
+        }
+        else
+        {
+            result.push_back('1');
+        }
+    }
+    return result;
+}
+
 int main()
 {
-    int n;
     cin >> n;
-    vector<int> sol(n);
-    long long sum = 0;
+    vector<string> bit;
+    for (int i = 0; i < pow(2, n); i++)
+    {
+        bit.push_back(to_binary(i));
+        // cout << bit[i] << endl;
+    }
+    int a[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> sol[i];
-        sum += sol[i];
+        cin >> a[i];
     }
-
-    sort(sol.begin(), sol.end());
-
-    for (auto x : sol)
+    long long int res = INT_MAX;
+    for (auto x : bit)
     {
-        cout << x << " ";
-    }
-    cout << endl;
-    long long sum1 = 0, sum2 = 0;
-
-    if (n % 2 == 0)
-    {
-        int i = 0;
-        for (i = 0; i < n / 4; i++)
+        long long int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < n; i++)
         {
-            // cout << sol[i] << "+" << sol[n - i - 1] << "=" << sol[i] + sol[n - i - 1] << endl;
-            sum1 = sum1 + sol[i] + sol[n - i - 1];
+            if (x[i] == '0')
+            {
+                sum1 += a[i];
+            }
+            else
+            {
+                sum2 += a[i];
+            }
         }
-        sum1 += sol[n / 2 - 1];
-        // cout << "c1 : " << sum1 << endl;
-
-        for (int j = i; j < n / 4 + i; j++)
-        {
-            //cout << sol[j] << "+" << sol[n - j - 1] << "=" << sol[j] + sol[n - j - 1] << endl;
-            sum2 = sum2 + sol[j] + sol[n - j - 1];
-        }
-        //cout << sol[n / 2] << endl;
-        sum2 += sol[n / 2];
-
-        cout << abs(sum1 - sum2);
-        // cout << "c2 : " << sum2 << endl;
+        res = min(res, abs(sum1 - sum2));
     }
-    else
-    {
-        for (int i = 0; i < n / 2; i += 2)
-        {
-            sum1 = sum1 + sol[i] + sol[n - i - 1];
-        }
-        sum2 = sum - sum1 - sol[n / 2];
-        // cout << sum << " " << sum1 << " " << sum2 << endl;
-        cout << abs(sum2 - sum1);
-    }
+    cout << res << endl;
 }
