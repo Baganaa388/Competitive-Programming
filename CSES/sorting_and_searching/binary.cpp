79aff61cea3900cc5773622670485ea349a4406d
Baganaa <baganaa237@gmail.com>
2026-05-02 09:39:26 +0800

    xxe

---
+++ CSES/sorting_and_searching/binary.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(x) x.begin(), x.end()
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int target;
+    // cout << "target : ";
+    cin >> target;
+    sort(all(a));
+    int count = 0;
+    int mid;
+    int l = 0, r = n;
+    while (l <= r)
+    {
+        count++;
+        mid = (r + l) / 2;
+        if (a[mid] > target)
+        {
+            r = mid - 1;
+        }
+        else if (a[mid] < target)
+        {
+            l = mid + 1;
+        }
+        else
+        {
+            cout << "found xD " << count;
+            break;
+        }
+    }
+}
